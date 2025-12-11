f13ae90766f53a18af00d7f2c97b1cd9268816dc
Hoiyu Yiu <61400426+hoiyuyiu@users.noreply.github.com>
2025-12-12 00:20:04 +0800

    Update Config

--- urodele.config.ts
+++ urodele.config.ts
@@ -1,18 +1,18 @@
 export const config = {
   github: {
-    login: "glink25", // github login name, not user name
-    repo: "test-for-anything", //"urodele",
+    login: "hoiyuyiu", // github login name, not user name
+    repo: "hoiyuyiu.github.io", //"urodele",
     logInUrl: "",
     logInAuthUrl: "",
   },
   head: {
-    title: "Urodele",
+    title: "Homare",
     brand: "Urodele",
     description: "A self-owned full-static blog system",
   },
   footer: {
-    copyright: "© Glink",
-    copyrightUrl: "https://github.com/glink25",
+    copyright: "© hoiyuyiu",
+    copyrightUrl: "https://github.com/hoiyuyiu",
   },
   pagination: {
     size: 10,
